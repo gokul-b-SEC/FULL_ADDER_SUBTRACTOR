c424c59db631c24b4f3173e67d991e3f1969467e
gokul-b-SEC <gokulbalaji2k4@gmail.com>
2025-12-20 16:33:02 +0530

    Add files via upload

---
+++ gokul EXP 4.1 PROGRAM.v
@@ -0,0 +1,11 @@
+FULL SUBTRACTOR
+
+module full_subtractor(diff, borrow, a, b, bin);
+  output diff;
+  output borrow;
+  input a;
+  input b;
+  input bin;
+  assign diff = a ^ b ^ bin;
+  assign borrow = (~a & b) | (~(a ^ b) & bin);
+endmodule
---
+++ gokul EXP 4.2 PROGRAM.v
@@ -0,0 +1,15 @@
+FULL ADDER
+
+module exp4(df,bo,a,b,bin);
+output df;
+output bo;
+input a;
+input b;
+input bin;
+wire w1,w2,w3;
+assign w1=a^b;
+assign w2=(~a&b);
+assign w3=(~w1&bin);
+assign df=w1^bin;
+assign bo=w2|w3;
+endmodule
